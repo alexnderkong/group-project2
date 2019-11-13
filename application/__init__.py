ff329995759ee4c7549d5f3091313fb0bef080df
EC2 Default User <ec2-user@ip-172-31-23-130.eu-west-1.compute.internal>
2019-11-13 11:47:43 +0000

    set up mysql rds

--- application/__init__.py
+++ application/__init__.py
@@ -5,10 +5,10 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 from os import getenv
 
-#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://groupproject:group-password@group-project.cs0cgwkolkmi.eu-west-1.rds.amazonaws.com/group-project'
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://groupproject:group-password@group-project.cs0cgwkolkmi.eu-west-1.rds.amazonaws.com/groupproject'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 app.config['SECRET_KEY'] = 'secretkey1234'
 db = SQLAlchemy(app)
